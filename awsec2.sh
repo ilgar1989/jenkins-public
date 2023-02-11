442c18cc35efa35876ed4d03ba7281d6b214b71c
ilgar1989 <123923589+ilgar1989@users.noreply.github.com>
2023-02-11 03:54:56 -0500

    Update awsec2.sh

--- awsec2.sh
+++ awsec2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-0aa7d40eeae50c9a9 --count1 --instance-type t2.micro --key-name 123 --region us-east-1
+aws ec2 run-instances --image-id ami-0aa7d40eeae50c9a9 --count 1 --instance-type t2.micro --key-name 123 --region us-east-1
