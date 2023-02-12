2eb328d262719f1522d3c9313aa0ed4ad635a2b8
ilgar1989 <123923589+ilgar1989@users.noreply.github.com>
2023-02-11 19:27:35 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,23 @@
-provider "aws" {
-  region  = "us-east-1"
+terraform {
+require_providers {
+  aws = {
+  source = "hashicorp/aws"
+  version = ">=3.34.0,<=3.35.0"
+  }
+  }
+  }
+  
+  provider "aws" {
+  region = "us-east-1"
   profile = "default"
-}
-
-resource "aws_instance" "my-first-ec2" {
-  ami           = "ami-0aa7d40eeae50c9a9"
+  }
+  
+  resource "aws_instance" "my-first-ec2" {
+  ami   = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
   tags = {
-    Name  = "myec2-1"
+    Name = "myec2-1"
     Owner = "ilgar"
-  }
-}
+    }
+    }
+  
